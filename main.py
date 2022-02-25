f35d0c54e73d2062084757783eb72e87d207444b
dqj5182 <dqj5182@psu.edu>
2022-02-25 11:44:11 +0900

    commit

--- main.py
+++ main.py
@@ -73,13 +73,13 @@ transform_train = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     CIFAR10Policy(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
     RandomCropPaste(size=32)
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
 ])
 
 batch_size = 128
